fdef47b837c055c007b81ccd2cc5df4cb9f78faf
revine kim <skyho31@gmail.com>
2018-09-20 00:01:43 +0900

    custom log

--- modalModule.js
+++ modalModule.js
@@ -36,24 +36,64 @@
  */
 
 (function (global) {
-  function Modal(options) {
-    this.type = options.type;
-    this.content = options.content;
-    this.title = options.title || 'alert';
-    this.closeLabel = options.closeLabel || 'cancel';
-    this.cssClass = options.cssClass || [];
-    this.buttons = options.buttons || [];
-    this.originOverflow = document.body.style.overflow;
+  
+  function domGenerator(tag, ...cssClass) {
+    const dom = document.createElement(tag);
+    cssClass.forEach(singleClass => {
+      if(singleClass !== undefined){
+        dom.classList.add(singleClass);
+      }
+    })
 
-
-    // function bind
-    this.show = this.show.bind(this);
-    this.hide = this.hide.bind(this);
+    return dom;
   }
+  
+  function customlog(message, type='info'){
+    console.log(type)
+    let logContainer;
+    let logBody;
 
-  Modal.prototype = {
-    constructor: Modal,
-    show: function () {
+    if(document.querySelector('.log-container')){
+      logContainer = document.querySelector('.log-container');
+      logBody = logContainer.childNodes[0];
+
+      logContainer.classList.add(type);
+      logContainer.classList.remove('hidden');
+      
+    } else {
+      logContainer = domGenerator('div', 'log-container', type);
+      logBody = domGenerator('div', 'log-body');
+
+      logContainer.appendChild(logBody);      
+      document.body.appendChild(logContainer);
+    }
+
+    logBody.innerHTML = message;
+    
+    setTimeout(function(){
+      logContainer.classList.add('hidden');
+    }, 1000);
+  }
+  
+
+  class Modal {
+    constructor(options){
+      this.type = options.type;
+      this.content = options.content;
+      this.title = options.title || 'alert';
+      this.closeLabel = options.closeLabel || 'cancel';
+      this.cssClass = options.cssClass || [];
+      this.buttons = options.buttons || [{
+        content: 'confirm'
+      }];
+      this.originOverflow = document.body.style.overflow;
+
+      // function bind
+      this.show = this.show.bind(this);
+      this.hide = this.hide.bind(this);
+    }
+
+    show() {
       this._create();
       this._bindEvent();
 
@@ -61,27 +101,31 @@
       this.modalBody.innerHTML = this.content;
 
       document.body.style.overflow = 'hidden';
-    },
-    hide: function () {
+    }
+
+    hide() {
       this.modalContainer.remove();
       if (this._isEmpty()) {
         document.body.style.overflow = this.originOverflow;
       }
-    },
+    }
 
     // private method
-    _create: function () {
-      this.modalContainer = this._domGenerator('div', 'modal-container', 'show');
-      this.modalWrapper = this._domGenerator('div', 'wrapper', 'dimmer');
-      this.modalDiv = this._domGenerator('div', 'modal', this.title);
-      this.modalTitle = this._domGenerator('div', 'modal-title');
-      this.modalBody = this._domGenerator('div', 'modal-body');
-      this.modalFooter = this._domGenerator('div', 'modal-footer');
-      this.cancelBtn = this._domGenerator('button', 'modal-btn', 'cancel');
+    _create(){
+      this.modalContainer = domGenerator('div', 'modal-container', 'show');
+      this.modalWrapper = domGenerator('div', 'wrapper', 'dimmer');
+      this.modalDiv = domGenerator('div', 'modal', this.title);
+      this.modalTitle = domGenerator('div', 'modal-title');
+      this.modalBody = domGenerator('div', 'modal-body');
+      this.modalFooter = domGenerator('div', 'modal-footer');
+      this.cancelBtn = domGenerator('button', 'modal-btn', 'cancel');
       this.cancelBtn.innerHTML = this.closeLabel;
 
-      if(this.type === 'confirm'){
-        this.confirmBtn = this._domGenerator('button', 'modal-btn', 'confirm');
+      if(this.type === 'confirm' && this.buttons.length === 0){
+        this.buttons.push({
+          content: 'confirm',
+          message: 'confirm'
+        });
       }
 
       this.cssClass.forEach(singleClass => {
@@ -99,37 +143,31 @@
       this.modalDiv.appendChild(this.modalFooter);
 
       document.body.appendChild(this.modalContainer);
-    },
-    _bindEvent: function () {
+    }
+
+    _bindEvent () {
       const that = this;
       this.modalDiv.addEventListener('click', function (e) {
         if (e.target.className === 'modal-btn cancel') {
           that.hide();
         }
       });
-    },
-    _createButton: function (option) {
+    }
+
+    _createButton(option) {
       const { content, cssClass, func } = option;
-      const button = this._domGenerator('button', 'modal-btn', cssClass);
+      const button = domGenerator('button', 'modal-btn', cssClass);
       button.innerText = content;
       button.onclick = (func && func.bind(this)) || this.hide;
 
       return button;
-    },
-    _domGenerator: function (tag, ...cssClass) {
-      const dom = document.createElement(tag);
-      cssClass.forEach(singleClass => {
-        if(singleClass !== undefined){
-          dom.classList.add(singleClass);
-        }
-      })
+    }
 
-      return dom;
-    },
-    _isEmpty: function () {
+    _isEmpty() {
       return document.getElementsByClassName('modal-container').length === 0;
     }
   }
 
   global.Modal = Modal;
+  global.customlog = customlog;
 }(window));
