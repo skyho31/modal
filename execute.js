1ecd5024cb8d99f24df3fd1bd192bd586b51d64b
shkim <shkim@n3n.co.kr>
2018-09-20 12:22:29 +0900

    complete

---
+++ execute.js
@@ -0,0 +1,112 @@
+/**
+ * Modal Sample Test File
+ * 
+ * Author : Seonho Kim 
+ * version : es6
+ * date : 2018.09.20
+ * 
+ * description : 개별 버튼에 해당하는 모달을 생성하고 이벤트를 할당한 후, 요구 스펙의 충족여부를 확인하기 위한 실행 테스트 샘플 파일
+ */
+
+// sample string
+const shortSample = `"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."`;
+const mediumSample = `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.`;
+const longSample = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+
+// 1. create modals
+const alert1Modal = new Modal({
+  content: longSample,
+  title: 'alert-1',
+  type: 'alert',
+  classes: ['alert'],
+  buttons: [{
+    content: 'heloo'
+  }]
+});
+const alert2Modal = new Modal({
+  content: mediumSample,
+  title: 'alert-2',
+  type: 'alert'
+});
+const alert3Modal = new Modal({
+  content: shortSample,
+  title: 'alert-3',
+  type: 'alert'
+});
+const confirm1Modal = new Modal({
+  content: longSample,
+  title: 'confirm-1',
+  type: 'confirm',
+  classes: ['confirm'],
+  buttons: [{
+      content: 'ok',
+      classes: ['hello'],
+      func: function () {
+        customlog('confirm1 ok');
+        this.hide();
+      }
+    },
+    {
+      content: 'warn',
+      func: function () {
+        customlog('alert', 'alert');
+      }
+    }
+  ]
+});
+const confirm2Modal = new Modal({
+  content: mediumSample,
+  title: 'confirm-2',
+  type: 'confirm',
+  // className: ['hello'],
+  buttons: [{
+    content: 'ok',
+    func: function () {
+      customlog('confirm2 ok');
+      this.hide();
+    }
+  }]
+});
+const confirm3Modal = new Modal({
+  content: shortSample,
+  title: 'confirm-3',
+  type: 'confirm',
+});
+
+
+// 2. event bind
+document.body.addEventListener('click', function (e) {
+  if (e.target.classList.contains('button-test')) {
+    switch (e.target.id) {
+      case 'alert-1':
+        alert1Modal.show();
+        break;
+
+      case 'alert-2':
+        alert1Modal.show();
+        alert2Modal.show();
+        break;
+
+      case 'alert-3':
+        alert1Modal.show();
+        alert2Modal.show();
+        alert3Modal.show();
+        break;
+
+      case 'confirm-1':
+        confirm1Modal.show();
+        break;
+
+      case 'confirm-2':
+        confirm1Modal.show();
+        confirm2Modal.show();
+        break;
+
+      case 'confirm-3':
+        confirm1Modal.show();
+        confirm2Modal.show();
+        confirm3Modal.show();
+        break;
+    }
+  }
+})
--- modalModule.js
+++ modalModule.js
@@ -3,89 +3,119 @@
  * 
  * Author : Seonho Kim 
  * version : es6
+ * date : 2018.09.20
  * 
- * example : 
- * modal = new Modal({
- *  content : 'contentSample',
- *  title : 'titleSample',
- *  type: 'confirm' or 'alert'           *default - alert
- *  cssClass: ['custom', 'class']        *default - [];
- *  closeLabel: 'close'                  *default - 'close'
- *  buttons : [                         
- *      {
- *        content : 'ok',
- *        cssClass : []
- *        func: function(){              *default - this.hide(); 
- *          console.log('helloworld'); 
- *          this.hide();
- *        }
- *      },
- *      {
- *        content : 'cancel',
- *        cssClass : []
- *        func: function(){              *default - this.hide(); 
- *          console.log('helloworld'); s
- *          this.hide();
- *        }
- *      }
- *    ]
- *  });
+ * 1.사용자에게 노출될 내용
+ * - Modal 제목
+ * - Modal 내용
+ * - Button : 1 ~ n개
  * 
- * modal.show();
- * modal.hide();
+ * 2.기능
+ * - parameter를 통해, modal 생성 가능
+ * - modal의 type, content, title 등 주요 내용 이외에도 Button이나 Modal에 custom class 배열의 형태로 부여가능
+ * - Modal 창의 높이는 Content에 따라 유동적으로 변화
+ * - 복수 개의 Modal Overlay 지원
+ * - Modal 시 Scroll 방지 지원
+ * - Custom Button 지원
+ *   설정한 타입 (alert, confirm)에 따라 설정할 수 있으며, 
+ *   1) type이 alert일 시, 추가적인 버튼을 할당할 수  없다. 다만 closeLabel의 변경을 통해, 노출되는 버튼의 text를 변경할 수 있다. 
+ *   2) type이 confirm일 경우, 추가적인 Button을 할당할 수 있다. 아무것도 할당하지 않을 시 기본적인 확인 버튼과 취소 버튼이 노출된다. 하나라도 옵션을 할당할 경우, 확인 버튼은 따로 생성되지 않는다. 
+ *   3) 추가적으로 유저가 옵션에 따라 버튼에 Modal을 닫는 기능을 부여할 수 있도록 this.close()를 Button의 func 속성 내부에 부여 시 닫는 동작을 컨트롤 할 수 있다. 
+ *   4) func 속성에 아무 것도 할당하지 않을 시 Button은 Modal을 닫는 용도로 사용된다.
+ *  
+ * 3.특이사항
+ * - confirm 시 알림창을 띄우기 위해 customlog function 사용
+ * 
+ * 
+ * 개별 function의 설명은 이하 주석 참조
  */
 
 (function (global) {
-  
-  function domGenerator(tag, ...cssClass) {
+
+  /**
+   * @description	 generate dom object using tag name and className
+   * 
+   * @param {String} tag html tag name
+   * @param  {...String} classes className Array
+   * @returns {HTMLElement} 
+   */
+  function domGenerator(tag, ...classes) {
     const dom = document.createElement(tag);
-    cssClass.forEach(singleClass => {
-      if(singleClass !== undefined){
+    classes.forEach(singleClass => {
+      if (singleClass !== undefined) {
         dom.classList.add(singleClass);
       }
     })
 
     return dom;
   }
-  
-  function customlog(message, type='info'){
-    console.log(type)
+
+
+  /**
+   * @description	print log message 
+   * 
+   * @param {String} message log message
+   * @param {String} type info || alert change border-left color
+   */
+  function customlog(message, type = 'info') {
+
+    const queryContainer = document.querySelector('.log-container');
+    const logBox = domGenerator('div', 'log-box', type);
+    const logBody = domGenerator('div', 'log-body');
     let logContainer;
-    let logBody;
 
-    if(document.querySelector('.log-container')){
-      logContainer = document.querySelector('.log-container');
-      logBody = logContainer.childNodes[0];
-
-      logContainer.classList.add(type);
-      logContainer.classList.remove('hidden');
-      
-    } else {
-      logContainer = domGenerator('div', 'log-container', type);
-      logBody = domGenerator('div', 'log-body');
-
-      logContainer.appendChild(logBody);      
+    // check isBuilt
+    if (!queryContainer) {
+      logContainer = domGenerator('div', 'log-container');
       document.body.appendChild(logContainer);
+    } else {
+      logContainer = queryContainer;
     }
 
+    // change message
     logBody.innerHTML = message;
-    
-    setTimeout(function(){
-      logContainer.classList.add('hidden');
+    logBox.appendChild(logBody);
+    logContainer.appendChild(logBox);
+
+    // disappear log
+    setTimeout(function () {
+      logBox.remove();
     }, 1000);
   }
-  
 
+
+  /**
+   * 
+   * @description modal module with overlay feature
+   * 
+   * @param {Object} options {
+   *  type: String,
+   *  content: String,
+   *  title: String,
+   *  confirmLabel: String,
+   *  closeLabel: String,
+   *  classes: Array,
+   *  buttons: [
+   *    {
+   *      content: String,
+   *      func: Function,
+   *      classes: Array
+   *    }
+   *  ]
+   * }
+   * 
+   */
   class Modal {
-    constructor(options){
-      this.type = options.type;
-      this.content = options.content;
-      this.title = options.title || 'alert';
+    constructor(options) {
+      // set initialValue
+      options = options || {};
+      this.type = options.type || 'alert';
+      this.content = options.content || 'no contents';
+      this.title = options.title || '';
+      this.confirmLabel = options.confirmLabel || 'confirm';
       this.closeLabel = options.closeLabel || 'cancel';
-      this.cssClass = options.cssClass || [];
-      this.buttons = options.buttons || [{
-        content: 'confirm'
-      }];
+      this.classes = options.classes || [];
+      this.buttons = this._splitButton(options.buttons || []);
       this.originOverflow = document.body.style.overflow;
 
       // function bind
@@ -97,74 +127,113 @@
       this._create();
       this._bindEvent();
 
-      this.modalTitle.innerHTML = this.title;
-      this.modalBody.innerHTML = this.content;
-
+      // change overflow attribute for scroll lock
       document.body.style.overflow = 'hidden';
     }
 
     hide() {
       this.modalContainer.remove();
       if (this._isEmpty()) {
+        // restore origin overflow attribute
         document.body.style.overflow = this.originOverflow;
       }
     }
 
-    // private method
-    _create(){
-      this.modalContainer = domGenerator('div', 'modal-container', 'show');
-      this.modalWrapper = domGenerator('div', 'wrapper', 'dimmer');
-      this.modalDiv = domGenerator('div', 'modal', this.title);
-      this.modalTitle = domGenerator('div', 'modal-title');
-      this.modalBody = domGenerator('div', 'modal-body');
-      this.modalFooter = domGenerator('div', 'modal-footer');
-      this.cancelBtn = domGenerator('button', 'modal-btn', 'cancel');
-      this.cancelBtn.innerHTML = this.closeLabel;
-
-      if(this.type === 'confirm' && this.buttons.length === 0){
-        this.buttons.push({
-          content: 'confirm',
-          message: 'confirm'
-        });
-      }
-
-      this.cssClass.forEach(singleClass => {
-        this.modalDiv.classList.add(singleClass);
-      });
-
-      this.type === 'confirm' && this.buttons.forEach(buttonOption => {
-        this.modalFooter.appendChild(this._createButton(buttonOption));
-      });
-      this.modalFooter.appendChild(this.cancelBtn);
-      this.modalContainer.appendChild(this.modalWrapper);
-      this.modalWrapper.appendChild(this.modalDiv);
-      this.modalDiv.appendChild(this.modalTitle);
-      this.modalDiv.appendChild(this.modalBody);
-      this.modalDiv.appendChild(this.modalFooter);
-
-      document.body.appendChild(this.modalContainer);
-    }
-
-    _bindEvent () {
+    _bindEvent() {
+      // for bind event scope
       const that = this;
-      this.modalDiv.addEventListener('click', function (e) {
-        if (e.target.className === 'modal-btn cancel') {
-          that.hide();
+
+      // event delegate
+      this.modalContainer.addEventListener('click', function (e) {
+        if (e.target.classList.contains('modal-btn')) {
+          const index = e.target.dataset.index;
+          const func = that.buttons.funcs[index];
+          
+          (func && func.bind(that) || that.hide)();
         }
       });
     }
 
-    _createButton(option) {
-      const { content, cssClass, func } = option;
-      const button = domGenerator('button', 'modal-btn', cssClass);
-      button.innerText = content;
-      button.onclick = (func && func.bind(this)) || this.hide;
+    _create() {
+      // create dom
+      this.modalContainer = domGenerator('div', 'modal-container');
+      const modalWrapper = domGenerator('div', 'wrapper', 'dimmer');
+      const modalDiv = domGenerator('div', 'modal');
+      const modalTitle = domGenerator('div', 'modal-title');
+      const modalBody = domGenerator('div', 'modal-body');
+      const modalFooter = domGenerator('div', 'modal-footer');
+
+      // bind value
+      modalTitle.innerHTML = this.title;
+      modalBody.innerHTML = this.content;
+
+      // apply custom css
+      this.classes.forEach(singleClass => {
+        modalDiv.classList.add(singleClass);
+      });
+
+      // add button group
+      this.buttons.texts.forEach((buttonText, idx) => {
+        const classes = this.buttons.classes[idx];
+        const button = this._createButton(buttonText, classes, idx);
+
+        modalFooter.appendChild(button);
+      });
+
+      // render dom
+      this.modalContainer.appendChild(modalWrapper);
+      modalWrapper.appendChild(modalDiv);
+      modalDiv.appendChild(modalTitle);
+      modalDiv.appendChild(modalBody);
+      modalDiv.appendChild(modalFooter);
+      document.body.appendChild(this.modalContainer);
+    }
+
+    _createButton(text, classes, idx) {
+      const button = domGenerator('button', 'modal-btn');
+      Array.isArray(classes) && classes.forEach(singleClass => {
+        button.classList.add(singleClass);
+      });
+
+      button.innerText = text;
+      button.dataset.index = idx;
 
       return button;
     }
 
     _isEmpty() {
       return document.getElementsByClassName('modal-container').length === 0;
+    }
+
+    _splitButton(buttonOptions) {
+      const buttonTexts = [];
+      const buttonFuncs = [];
+      const buttonClasses = [];
+
+      if (this.type === 'confirm') {
+        if (buttonOptions.length > 0) {
+          buttonOptions.forEach(v => {
+            buttonTexts.push(v.content || '');
+            buttonFuncs.push(v.func);
+            buttonClasses.push(v.classes);
+          });
+        } else {
+          buttonTexts.push(this.confirmLabel);
+          buttonFuncs.push(function () {
+            customlog('confirm');
+            this.hide();
+          });
+        }
+      }
+
+      // add cancel button
+      buttonTexts.push(this.closeLabel);
+
+      return {
+        texts: buttonTexts,
+        funcs: buttonFuncs,
+        classes: buttonClasses
+      }
     }
   }
 
